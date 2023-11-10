4a68eb2a888a58a0644159cbb1999adff66fdf00
val <erguotou200@gmail.com>
2023-11-09 21:30:48 -0500

    python

--- img_downloader.py
+++ img_downloader.py
@@ -10,49 +10,11 @@ import csv
 # test save catche
 # url = 'https://3dmodels.org/360-view/?id=235798'
 # 设置保存路径
-list_path = r'/home/kyber/Pictures/crawler2/url_list.csv'
-dl_path = r'/home/kyber/Pictures/crawler2/'
-
-# Create the parent directory if it doesn't exist
-os.makedirs(dl_path, exist_ok=True)
+list_path = r'/home/val/Pictures/crawler2/url_list.csv'
+dl_path = r'/home/val/Pictures/crawler2/'
+progress_path = r'/home/val/Pictures/crawler2/checker.txt'
 
 
-user_agent = [
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
-    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
-    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
-    "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
-    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
-    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
-    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
-    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
-    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
-    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
-    ]
-headers = {
-        "User-Agent": random.choice(user_agent),
-        "Referer": "https://3dmodels.org/3d-models/lexus-tx-premium-us-spec-2023"
-    }
-
-# Open the file in read mode
-
-full_list = []
-if os.path.exists(list_path):
-    try:
-        with open(list_path, mode='r', newline='') as file:
-            reader = csv.reader(file)
-            header = next(reader)  # Read the header row
-            for row in reader:
-                data_list.append(row)
-    except Exception as e:
-        print(f"An error occurred while reading from CSV: {e}")
-
-else:
-    print("LIST FILE DOES NOT EXIST")
 
 
 def dl_img(url_list, car_path):
@@ -68,7 +30,7 @@ def dl_img(url_list, car_path):
             # Extract the image data
             image_data = response.content
 
-            img_name = url.split("/")[-1]
+            img_name = url.split("-")[-1]
 
             # Specify the file path within the folder using the extracted file name
             file_path = os.path.join(car_path, img_name)
@@ -80,8 +42,7 @@ def dl_img(url_list, car_path):
             print(f"Error downloading image for {title}: {e}")
             
 
-for row in full_list: 
-    serial, status, title, url = row
+
 
 def strip_imgurl(title, url):
     # Create a folder with the title as its name
@@ -124,4 +85,60 @@ def strip_imgurl(title, url):
     except requests.exceptions.RequestException as e:
         print(f"Error downloading image for {title}: {e}")
         
+# Create the parent directory if it doesn't exist
+os.makedirs(dl_path, exist_ok=True)
+
+
+user_agent = [
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
+    "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
+    ]
+headers = {
+        "User-Agent": random.choice(user_agent),
+        "Referer": "https://3dmodels.org/3d-models/lexus-tx-premium-us-spec-2023"
+    }
+
+# Open the file in read mode
+row_to_read = 1  # Adjust as needed
+
+
+# Read the last processed line number from the progress file
+try:
+    with open(progress_path, 'r') as pf:
+        last_processed_line = int(pf.readline().strip())
+except FileNotFoundError:
+    last_processed_line = 0
+# Determine whether to start from the beginning or resume
+if last_processed_line <= 0:
+    print(f"Starting from the beginning for file: {list_path}")
+else:
+    print(f"Resuming from line {last_processed_line} for file: {list_path}")
+# Open the CSV file and process each row
+with open(list_path, 'r', newline='') as file:
+    reader = csv.reader(file)
+    
+    for line_num, row in enumerate(reader, start=1):
+        if line_num <= last_processed_line:
+            continue  # Skip rows that have already been processed
         
+        # Perform your processing on the row here
+        serial, status, title, url = row
+        strip_imgurl(title, url)
+        
+        # Update the progress file with the current line number
+        with open(progress_path, 'w') as pf:
+            pf.write(str(line_num))
+
+
