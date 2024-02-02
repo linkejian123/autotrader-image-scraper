8b9dbe71f6aaa575399bc404d0e11c48a147ae1c
val <erguotou200@gmail.com>
2024-02-01 23:17:15 -0500

    need python 3.10

--- obja-dl/obja-gen.py
+++ obja-dl/obja-gen.py
@@ -1,38 +1,64 @@
 import bpy
 import os
+import math
 
-path_to_glb_folder = "/data/waffle1/crawl/objaverse/hf-objaverse-v1/glbs/000-000/" # If you are using Windows use r"\Users\Path\To\Folder"
-path_to_jpeg_folder = "/data/waffle1/crawl/objaverse/images/"
+path_to_glb_folder = "/home/val/tryout/obja/glb/volkswagen-golf-gti-free-realistic/source/"
+path_to_jpeg_folder = "/home/val/tryout/obja/images/"
+num_photos = 6  # Number of photos to capture (360 degrees / 60 degrees interval)
 
 def deleteAllObjects():
     """
     Deletes all objects in the current scene
     """
-    deleteListObjects = ['MESH', 'CURVE', 'SURFACE', 'META', 'FONT', 'HAIR', 'POINTCLOUD', 'VOLUME', 'GPENCIL',
-                         'ARMATURE', 'LATTICE', 'EMPTY', 'LIGHT', 'LIGHT_PROBE', 'CAMERA', 'SPEAKER']
-
-    # Select all objects in the scene to be deleted:
-
-    for o in bpy.context.scene.objects:
-        for i in deleteListObjects:
-            if o.type == i:
-                o.select_set(False)
-            else:
-                o.select_set(True)
-    bpy.ops.object.delete() # Deletes all selected objects in the scene
+    bpy.ops.object.select_all(action='DESELECT')
+    bpy.ops.object.select_by_type(type='MESH')
+    bpy.ops.object.delete()
 
 deleteAllObjects()
 
-glb_dirList = os.listdir(path_to_glb_folder)
-
-removeList = [".gitignore", ".DS_Store"] # If you have . files in your directory
-glb_dirList = [x for x in glb_dirList if (x not in removeList)]
+glb_dirList = [f for f in os.listdir(path_to_glb_folder) if f.lower().endswith('.glb')]
 
 for i in glb_dirList:
-    bpy.ops.import_scene.gltf(filepath=os.path.join()) # Import .glb file to scene
+    print(i)
+    bpy.ops.import_scene.gltf(filepath=os.path.join(path_to_glb_folder, i))  # Import .glb file to scene
 
-    bpy.context.scene.render.filepath = path_to_jpeg_folder # Set save path for images
-    bpy.context.scene.render.image_settings.file_format = "JPEG" # Set image file format
-    bpy.ops.render.render(write_still=True) # Tell Blender to render an image
+    # Add light (Point light with increased energy)
+    bpy.ops.object.light_add(type='POINT', location=(0, 0, 10))
+    light_object = bpy.context.object
+    light_object.data.energy = 1000  # Adjust the energy value
+
+    # Set up camera with wide angle (focal length 20mm)
+    bpy.ops.object.camera_add(location=(7, 7, 7), rotation=(math.radians(45), 0, math.radians(45)))
+    scene = bpy.context.scene
+    camera = bpy.context.object
+    camera.data.lens = 20  # Set focal length to 20mm
+    scene.camera = camera
+
+    # Set render settings
+    bpy.context.scene.render.filepath = os.path.join(path_to_jpeg_folder, i.replace('.glb', '_'))  # Set save path for images
+    bpy.context.scene.render.image_settings.file_format = "JPEG"  # Set image file format
+
+    # Adjust exposure and contrast in camera settings
+    bpy.context.scene.view_settings.view_transform = 'Filmic'
+    bpy.context.scene.view_settings.look = 'High Contrast'
+    
+    # Set the number of samples for Cycles render engine
+    bpy.context.scene.cycles.samples = 500  # Adjust the number of render samples for Cycles
+    
+    # Set the number of samples for Eevee render engine
+    # bpy.context.scene.eevee.taa_render_samples = 16  # Adjust the number of render samples for Eevee
+
+    # Animate the camera in a circular motion
+    for frame in range(num_photos):
+        angle = frame * (2 * math.pi) / num_photos
+        camera.location = (5 * math.cos(angle) + 7, 5 * math.sin(angle) + 7, 5)
+        camera.rotation_euler = (math.radians(45), 0, math.radians(45) - angle)
+        bpy.context.scene.frame_set(frame + 1)
+        
+        # Construct the file name using the glb name and photo number
+        file_name = "{}_{}.jpg".format(i.replace('.glb', ''), frame + 1)
+        bpy.context.scene.render.filepath = os.path.join(path_to_jpeg_folder, file_name)
+
+        bpy.ops.render.render(write_still=True)  # Render image for each frame
 
     deleteAllObjects()
